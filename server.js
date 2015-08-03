3c6536136bfa03d99601e8c51612628cbd2b1615
Caleb De La Cruz <cdelacruz@simetricaconsulting.com>
2015-08-03 19:28:41 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var http = require('http');
+var path = require('path');
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname, 'public/app')));
+router.use('/bower_components', express.static(path.resolve(__dirname, 'public/bower_components')));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
